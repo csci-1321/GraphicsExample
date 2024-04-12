b159c7dd17a93b5e3c9bd556fe26811c3710d67f
DrBrittonHorn <bhorn@trinity.edu>
2024-04-12 10:25:39 -0500

    adding map stuff

--- app/src/main/java/collections/TextAnalysis.java
+++ app/src/main/java/collections/TextAnalysis.java
@@ -1,13 +1,12 @@
 package collections;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class TextAnalysis {
     public static void main(String[] args) {
         Set<String> set = new HashSet<String>();
+        HashMap<String,Integer> freqs = new HashMap<String,Integer>();
 
         try {
         File f = new File("tempest.txt");
@@ -18,11 +17,26 @@ public class TextAnalysis {
             String[] parts = line.split("\\s");
             for (int i = 0; i != parts.length; ++i) {
                 parts[i] = parts[i].replaceAll("[^A-Za-z]", "");
-                set.add(parts[i].toLowerCase());
+                String wordToAdd = parts[i].toLowerCase();
+                set.add(wordToAdd);
+                freqs.putIfAbsent(wordToAdd, 0);
+                freqs.put(wordToAdd, freqs.get(wordToAdd) + 1);
+            }
+        }
+
+        String maxWord = null;
+        Integer maxFreq = 0;
+        for(Map.Entry<String,Integer> entry: freqs.entrySet()) {
+            if (entry.getKey().equals("") || entry.getKey().equals(" ")) continue;
+            if (entry.getValue() > maxFreq) {
+                maxFreq = entry.getValue();
+                maxWord = entry.getKey();
             }
         }
         System.out.println(set);
         System.out.println(set.size());
+        System.out.println(freqs);
+        System.out.println("max word: " + maxWord + ", frequency: " + freqs.get(maxWord));
         } catch (Exception e) {
             System.out.println("whoops");
         }
